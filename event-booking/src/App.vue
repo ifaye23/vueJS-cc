c6be30c0b7bb0ddfbfedf8a18868f870323c1495
ifaye23 <idrissa.faye.a@gmail.com>
2025-02-15 19:02:21 +0100

    defineProps method

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -7,7 +7,7 @@ import EventCard from './components/EventCard.vue';
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
         <section class="grid grid-cols-2 gap-8">
-            <EventCard v-for="i in 8" :key="i"/>
+            <EventCard v-for="i in 8" :key="i" title="Boombox Master" date="29-06-2003" description="Welcome to the DJs"/>
         </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
---
+++ event-booking/src/components/EventCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-zinc-800 border border-amber-600 rounded-md">
+        <h2 class="text-xl font-medium p-4 border-b border-amber-600">{{title}}</h2>
+        <p class="border-b border-amber-600 p-4 text-sky-400">{{date}}</p>
+        <p class="p-4 text-lime-200">{{description}}</p>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        title: String,
+        date: String,
+        description: String,
+    })
+</script>
