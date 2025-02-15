e9234b7fb08ba03efeb7092a3d8007bc28d9a7ea
ifaye23 <idrissa.faye.a@gmail.com>
2025-02-15 19:33:18 +0100

    emit events

--- event-booking/src/components/EventCard.vue
+++ event-booking/src/components/EventCard.vue
@@ -2,7 +2,14 @@
     <div class="bg-zinc-800 border border-amber-600 rounded-md">
         <h2 class="text-xl font-medium p-4 border-b border-amber-600">{{title}}</h2>
         <p class="border-b border-amber-600 p-4 text-sky-400">{{date}}</p>
-        <p class="p-4 text-lime-200">{{description}}</p>
+        <p class="p-4 text-lime-200 border-b border-amber-600">{{description}}</p>
+        <section class="flex justify-end p-4">
+            <button class="bg-amber-700  
+                text-xs py-2 px-4 rounded-sm 
+                hover:bg-orange-700 hover:text-sm hover:font-bold transition-all" 
+                @click="$emit('book')">Book event !
+            </button>
+        </section>
     </div>
 </template>
 
@@ -12,4 +19,6 @@
         date: String,
         description: String,
     })
+
+    defineEmits(['book'])
 </script>
