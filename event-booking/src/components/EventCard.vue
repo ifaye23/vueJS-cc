c67d583158e8605bd06dc00f18714f9f7b79cf40
ifaye23 <idrissa.faye.a@gmail.com>
2025-02-15 19:54:22 +0100

    button component

--- event-booking/src/components/EventCard.vue
+++ event-booking/src/components/EventCard.vue
@@ -4,21 +4,17 @@
         <p class="border-b border-amber-600 p-4 text-sky-400">{{date}}</p>
         <p class="p-4 text-lime-200 border-b border-amber-600">{{description}}</p>
         <section class="flex justify-end p-4">
-            <button class="bg-amber-700  
-                text-xs py-2 px-4 rounded-sm 
-                hover:bg-orange-700 hover:text-sm hover:font-bold transition-all" 
-                @click="$emit('book')">Book event !
-            </button>
+            <BookButton label="Book Event !"/>
         </section>
     </div>
 </template>
 
 <script setup>
+    import BookButton from '@/components/BookButton.vue';
+
     defineProps({
         title: String,
         date: String,
         description: String,
     })
-
-    defineEmits(['book'])
 </script>
