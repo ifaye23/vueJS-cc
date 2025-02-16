24d90e60a3b7fd9a6456dfff926258dc82ddffba
ifaye23 <idrissa.faye.a@gmail.com>
2025-02-16 01:54:35 +0100

    json-server setup

---
+++ event-booking/src/components/SectionCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <BaseCard>
+    <h2 v-if="$slots.header" class="text-xl font-medium p-4 border-b border-amber-600">
+      <slot name="header"></slot>
+    </h2>
+    <div v-if="$slots.date" class="p-4 text-sky-400 border-b border-amber-600">
+      <slot name="date"></slot>
+    </div>
+    <div class="p-4">
+      <slot></slot>
+    </div>
+  </BaseCard>
+</template>
+
+<script setup>
+import BaseCard from '@/components/BaseCard.vue';
+</script>
