68776c532a13abaea5e2f124a36f16db8168c7c5
ifaye23 <idrissa.faye.a@gmail.com>
2025-02-18 17:51:12 +0100

    Optimistic UI pt.1 : Bookinges

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -24,6 +24,7 @@
           v-for="book in bookings"
           :key="book.id"
           :eventTitle="book.eventTitle"
+          :status="book.status"
           @cancel="console.log('The booking has been canceled')"
         >
         </BookingCard>
@@ -48,34 +49,52 @@ const isLoadingBooking = ref(false);
 
 const bookings = ref([]);
 
-const fetchBookings = async () => {
-  isLoadingBooking.value = true;
-  try {
-    const response = await fetch('http://localhost:3001/bookings');
-    bookings.value = await response.json();
-    bookings.value.reverse();
-  } finally {
-    isLoadingBooking.value = false;
-  }
-};
-
 const addBooking = async (event) => {
   const newBooking = {
     id: Date.now().toString(),
     userId: 1,
     eventId: event.id,
     eventTitle: event.title,
+    status: 'pending',
   };
+
+  if (
+    bookings.value.some(
+      (booking) => booking.eventId === newBooking.eventId && booking.userId === newBooking.userId,
+    )
+  ) {
+    alert('Already registered for this event.');
+    return;
+  }
 
   bookings.value.unshift(newBooking);
 
   try {
-    isLoadingBooking.value = true;
-    await fetch('http://localhost:3001/bookings', {
+    const response = await fetch('http://localhost:3001/bookings', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ ...newBooking, status: 'confirmed' }),
     });
+    if (response.ok) {
+      const index = bookings.value.findIndex((book) => book.id === newBooking.id);
+      if (index !== -1) bookings.value[index].status = 'confirmed';
+    } else {
+      throw new Error('Failed to confirm booking');
+    }
+  } catch (error) {
+    // handle error
+    const index = bookings.value.findIndex((book) => book.id === newBooking.id);
+    if (index !== -1) bookings.value[index].status = 'failed';
+    console.error(`An error has occurred : ${error}`);
+  }
+};
+
+const fetchBookings = async () => {
+  isLoadingBooking.value = true;
+  try {
+    const response = await fetch('http://localhost:3001/bookings');
+    bookings.value = await response.json();
+    bookings.value.reverse();
   } finally {
     isLoadingBooking.value = false;
   }
--- event-booking/src/components/BookingCard.vue
+++ event-booking/src/components/BookingCard.vue
@@ -1,7 +1,70 @@
 <template>
   <SectionnedCard>
     <div class="flex flex-row justify-between items-center">
-      <p>{{ eventTitle }}</p>
+      <div class="flex gap-3 items-center">
+        <p>{{ eventTitle }}</p>
+        <svg
+          v-if="status === 'confirmed'"
+          class="size-5"
+          viewBox="0 0.5 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M3 12C3 4.5885 4.5885 3 12 3C19.4115 3 21 4.5885 21 12C21 19.4115 19.4115 21 12 21C4.5885 21 3 19.4115 3 12Z"
+              stroke="#FCF596"
+              stroke-width="2"
+            ></path>
+            <path
+              d="M9 12L10.6828 13.6828V13.6828C10.858 13.858 11.142 13.858 11.3172 13.6828V13.6828L15 10"
+              stroke="#FCF596"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </g>
+        </svg>
+        <svg
+          v-else-if="status === 'failed'"
+          class="size-5"
+          viewBox="0 -1 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M12 15H12.01M12 12V9M4.98207 19H19.0179C20.5615 19 21.5233 17.3256 20.7455 15.9923L13.7276 3.96153C12.9558 2.63852 11.0442 2.63852 10.2724 3.96153L3.25452 15.9923C2.47675 17.3256 3.43849 19 4.98207 19Z"
+              stroke="#EB5B00"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </g>
+        </svg>
+        <svg
+          v-else
+          class="animate-spin size-5"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M20.0001 12C20.0001 13.3811 19.6425 14.7386 18.9623 15.9405C18.282 17.1424 17.3022 18.1477 16.1182 18.8587C14.9341 19.5696 13.5862 19.9619 12.2056 19.9974C10.825 20.0328 9.45873 19.7103 8.23975 19.0612"
+              stroke="#FFB200"
+              stroke-width="2.5"
+              stroke-linecap="round"
+            ></path>
+          </g>
+        </svg>
+      </div>
       <BaseButton variant="danger">Cancel</BaseButton>
     </div>
   </SectionnedCard>
@@ -15,5 +78,9 @@ import BaseButton from '@/components/BaseButton.vue';
 defineEmits(['cancel']);
 defineProps({
   eventTitle: String,
+  status: {
+    type: String,
+    default: 'pending',
+  },
 });
 </script>
