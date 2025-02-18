8c73d054fc64e4d37e0e7f2b9aa7541b22a34864
ifaye23 <idrissa.faye.a@gmail.com>
2025-02-18 19:03:45 +0100

    components separation : eventlist

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -1,22 +1,7 @@
 <template>
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
-    <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8">
-      <template v-if="!isLoadingEvent">
-        <EventCard
-          v-for="event in events"
-          :key="event.id"
-          :title="event.title"
-          :date="event.date"
-          :description="event.description"
-          @book="addBooking(event)"
-        />
-      </template>
-      <template v-else>
-        <LoadingEventCard v-for="i in 4" :key="i" />
-      </template>
-    </section>
+    <EventList @book="addBooking($event)" />
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="flex flex-col gap-5">
       <template v-if="!isLoadingBooking">
@@ -39,12 +24,9 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import BookingCard from '@/components/BookingCard.vue';
-import EventCard from '@/components/EventCard.vue';
-import LoadingEventCard from '@/components/LoadingEventCard.vue';
+import EventList from '@/components/EventList.vue';
 import LoadingBookingCard from '@/components/LoadingBookingCard.vue';
 
-const events = ref([]);
-const isLoadingEvent = ref(false);
 const isLoadingBooking = ref(false);
 
 const bookings = ref([]);
@@ -121,18 +103,7 @@ const fetchBookings = async () => {
   }
 };
 
-const fetchEvents = async () => {
-  isLoadingEvent.value = true;
-  try {
-    const response = await fetch('http://localhost:3001/events');
-    events.value = await response.json();
-  } finally {
-    isLoadingEvent.value = false;
-  }
-};
-
 onMounted(() => {
-  fetchEvents();
   fetchBookings();
 });
 </script>
