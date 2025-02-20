74b3f1b0b8553354d99a8c7a985d3de0402a8317
ifaye23 <idrissa.faye.a@gmail.com>
2025-02-20 16:54:04 +0100

    types with typescript

--- vue-settings/src/App.vue
+++ vue-settings/src/App.vue
@@ -2,4 +2,24 @@
   <main>Hello world !</main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Tab, TabKey } from '@/types.ts';
+import { ref } from 'vue';
+
+const currentTab = ref<TabKey>('General');
+
+const tabs: Tab[] = [
+  {
+    key: 'General',
+    label: 'General',
+  },
+  {
+    key: 'Notifications',
+    label: 'Notifications',
+  },
+  {
+    key: 'Privacy',
+    label: 'Privacy',
+  },
+];
+</script>
---
+++ vue-settings/src/components/TabLink.vue
@@ -0,0 +1,15 @@
+<template>Link</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  currentTab: string;
+  tab: {
+    key: string;
+    label: string;
+  };
+}>();
+
+defineEmits<{
+  click: [];
+}>();
+</script>
