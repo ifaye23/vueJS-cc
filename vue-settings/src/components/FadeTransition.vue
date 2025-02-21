c7cd6ca6e54e358d1dd1a98d23459066ea3bf29e
ifaye23 <idrissa.faye.a@gmail.com>
2025-02-22 00:52:32 +0100

    added general page content

--- vue-settings/src/components/FadeTransition.vue
+++ vue-settings/src/components/FadeTransition.vue
@@ -7,16 +7,14 @@
 <style scoped>
 .fade-enter-from {
   opacity: 0;
-  transform: translate(30px);
 }
 
 .fade-leave-to {
   opacity: 0;
-  transform: translate(-30px);
 }
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: all 0.2s ease-out;
+  transition: all 0.15s ease-out;
 }
 </style>
