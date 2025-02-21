cb041f948928080c90f19d03eb74bc4662c989fc
ifaye23 <idrissa.faye.a@gmail.com>
2025-02-21 15:50:53 +0100

    quickfix : ul in navbar

--- vue-settings/src/components/Navbar.vue
+++ vue-settings/src/components/Navbar.vue
@@ -1,13 +1,16 @@
 <template>
-  <section class="flex bg-neutral-800 justify-around mb-10">
-    <TabLink
-      v-for="tab in tabs"
-      :current-tab="currentTab"
-      :tab-key="tab.key"
-      :label="tab.label"
-      @click="toggleTab(tab.key)"
-    >
-    </TabLink>
+  <section>
+    <ul class="flex flex-row bg-neutral-800 justify-around mb-10">
+      <li v-for="tab in tabs" :key="tab.key">
+        <TabLink
+          :current-tab="currentTab"
+          :tab-key="tab.key"
+          :label="tab.label"
+          @click="toggleTab(tab.key)"
+        >
+        </TabLink>
+      </li>
+    </ul>
   </section>
 </template>
 
