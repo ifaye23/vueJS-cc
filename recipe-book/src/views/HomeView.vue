97239379c14c35257e2148f2ba70dc4f8b1e4924
ifaye <ifaye@insa-rouen.fr>
2025-02-25 16:04:45 +0100

    error path handling

--- recipe-book/src/views/HomeView.vue
+++ recipe-book/src/views/HomeView.vue
@@ -11,6 +11,9 @@
         <li>
           <RouterLink :to="{ name: 'recipe', params: { id: 9000 } }">Recipe 9000</RouterLink>
         </li>
+        <li>
+          <RouterLink :to="'not-found'">Error</RouterLink>
+        </li>
       </ul>
     </nav>
   </div>
---
+++ recipe-book/src/views/NotFoundView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>404, Page not found</h2>
+    <p>You searched for : {{ $route.params.pathMatch }}</p>
+  </div>
+</template>
