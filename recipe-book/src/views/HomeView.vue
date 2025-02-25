8e7d4d0d7fc42ea7456b7f78bc13b882e106680e
ifaye <ifaye@insa-rouen.fr>
2025-02-25 16:12:18 +0100

    fixed param name break

--- recipe-book/src/views/HomeView.vue
+++ recipe-book/src/views/HomeView.vue
@@ -12,7 +12,7 @@
           <RouterLink :to="{ name: 'recipe', params: { id: 9000 } }">Recipe 9000</RouterLink>
         </li>
         <li>
-          <RouterLink :to="'not-found'">Error</RouterLink>
+          <RouterLink :to="{ name: 'not-found' }">Error</RouterLink>
         </li>
       </ul>
     </nav>
